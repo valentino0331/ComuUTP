e22dbbf30c644b35bfb4b4d061815f86dab6378a
Iam_Valentino0331 <marca0331@outlook.es>
2026-04-11 18:25:26 -0500

    Fix nodemailer typo: createTransporter -> createTransport

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 // Configurar transporter de nodemailer
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
