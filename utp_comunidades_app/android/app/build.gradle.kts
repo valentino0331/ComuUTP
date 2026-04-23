aace809a300921357dc92af597b275a4210de52f
Developer <dev@example.com>
2026-04-23 18:57:43 -0500

    Configurar Android para APK: nombre app, permisos internet, applicationId real

--- utp_comunidades_app/android/app/build.gradle.kts
+++ utp_comunidades_app/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.utp_comunidades_app"
+    namespace = "com.utpcomunidades.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,8 +21,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.utp_comunidades_app"
+        applicationId = "com.utpcomunidades.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
