9a5904fefd4bd808ca84bcfa207c56b1ad8f02c4
Developer <dev@example.com>
2026-04-23 20:20:08 -0500

    Corregir icono a icon2.png, volver a applicationId original para Firebase funcionar

--- utp_comunidades_app/android/app/build.gradle.kts
+++ utp_comunidades_app/android/app/build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.utpcomunidades.app"
+    namespace = "com.example.utp_comunidades_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,7 +21,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.utpcomunidades.app"
+        applicationId = "com.example.utp_comunidades_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
