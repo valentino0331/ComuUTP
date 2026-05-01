b66af221a2c9ab8904693e819cad1ac83fd7cccb
Developer <dev@example.com>
2026-05-01 18:46:47 -0500

    Fix: Add missing Provider import for Consumer and context.read

--- utp_comunidades_app/lib/screens/study_hub_screen.dart
+++ utp_comunidades_app/lib/screens/study_hub_screen.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:rive/rive.dart' hide LinearGradient;
 import 'package:file_picker/file_picker.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
